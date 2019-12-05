17fee6d3e3ce42b5cc797ff9da0e81dc11518488
theingredients <theingredientscollective@gmail.com>
2019-12-05 11:56:41 -0800

    added and updated files to help improve areas based off of Lighthouse

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,24 @@ module.exports = {
   pathPrefix: "/gatsby_pandas",
   siteMetadata: {
     title: `Panda Eating Lots*`,
+    description: `A simple description about pandas eating lots...`,
+    author: `jeromedagustin`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -11,6 +27,8 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
